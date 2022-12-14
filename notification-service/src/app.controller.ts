ae7cc10fe1764c221ad0d6f5d82e0fe7fdf8abac
Leonardo Almeida Cunha <lalmcunha@gmail.com>
2022-12-14 17:46:47 -0300

    Finaliza aula 01

--- notification-service/src/app.controller.ts
+++ notification-service/src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
-import { IAppService } from './app.service';
+import { CreateNotificationBody } from './create-notification-body';
+import { PrismaService } from './prisma.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
 
-@Controller('app2')
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: IAppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
-  @Get('hello')
-  getHello(): string {
-    return this.appService.getHello();
+  @Get()
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
   }
 }
--- notification-service/src/app.module.ts
+++ notification-service/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService2, IAppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
 // Isso aqui seria meu modelo? (entidade, objecto de dados, sei lá...)
 // Ou seria algo de arquitetura de domínios como foi no bootcamp?
@@ -8,11 +8,6 @@ import { AppService2, IAppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [
-    {
-      provide: IAppService,
-      useClass: AppService2,
-    },
-  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
