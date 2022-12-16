15ebce83b6831b84e4cd46229793c6f3dda4e1c1
Leonardo Almeida Cunha <lalmcunha@gmail.com>
2022-12-16 16:05:16 -0300

    Ajustes finais

--- notification-service/src/infra/http/http.module.ts
+++ notification-service/src/infra/http/http.module.ts
@@ -1,11 +1,23 @@
+import { UnreadNotification } from './../../application/use-cases/unread-notification';
+import { CancelNotification } from './../../application/use-cases/cancel-notification';
 import { SendNotification } from '@application/use-cases/send-notification';
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notifications';
+import { ReadNotification } from '@application/use-cases/read-notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}
