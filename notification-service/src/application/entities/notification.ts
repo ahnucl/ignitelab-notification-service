0e13bcabb7939334dda1a683509aa9d0ad37852a
Leonardo Almeida Cunha <lalmcunha@gmail.com>
2022-12-16 12:30:06 -0300

    Cria use-case de cancelar notificação

--- notification-service/src/application/entities/notification.ts
+++ notification-service/src/application/entities/notification.ts
@@ -7,6 +7,7 @@ export interface NotificationProps {
   content: Content;
   category: string;
   readAt?: Date | null;
+  canceledAt?: Date | null;
   createdAt: Date;
 }
 
@@ -67,5 +68,13 @@ export class Notification {
   // Não deve ter setter para esse campo
   public get createdAt(): Date {
     return this.props.createdAt;
+  }
+
+  public get canceledAt(): Date | null | undefined {
+    return this.props.canceledAt;
+  }
+
+  public cancel() {
+    this.props.canceledAt = new Date();
   }
 }
